15fca66ae41fec886dea1fc58b919c1535d521b6
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-04-25 20:01:52 +0300

    add recommended section title

---
+++ src/components/Recommended/Recommended.jsx
@@ -0,0 +1,11 @@
+import css from "./Recommended.module.css";
+
+const Recommended = () => {
+  return (
+    <div className={css.recomWrapper}>
+      <h2 className={css.recomTtl}>Recommended</h2>
+    </div>
+  );
+};
+
+export default Recommended;
--- src/pages/RecommendedPage/RecommendedPage.jsx
+++ src/pages/RecommendedPage/RecommendedPage.jsx
@@ -1,17 +1,21 @@
+import css from "./RecommendedPage.module.css";
+
 import AppDescription from "../../components/AppDescription/AppDescription";
 import Dashboard from "../../components/Dashboard/Dashboard";
 import Filters from "../../components/Filters/Filters";
 import Quote from "../../components/Quote/Quote";
+import Recommended from "../../components/Recommended/Recommended";
 
 const RecommendedPage = () => {
   return (
-    <>
+    <div className={css.pageWrapper}>
       <Dashboard>
         <Filters />
         <AppDescription />
         <Quote />
       </Dashboard>
-    </>
+      <Recommended />
+    </div>
   );
 };
 
