71eea3c56932781e29989258a7901d8c653d9945
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-04-30 18:10:38 +0300

    add library section

---
+++ src/components/Library/Library.jsx
@@ -0,0 +1,28 @@
+import css from "./Library.module.css";
+
+const Library = () => {
+  return (
+    <div className={css.libWrapper}>
+      <div className={css.titleWrapper}>
+        <h2 className={css.libTtl}>My library</h2>
+        <select name="" id=""></select>
+      </div>
+      <div className={css.libContent}>
+        <div className={css.imgWrapper}>
+          <img
+            className={css.image}
+            src="../../assets/images/books-desktop@1x.png"
+            alt="Books image"
+          />
+        </div>
+        <p className={css.libText}>
+          To start training, add{" "}
+          <span className={css.span}>some of your books</span> or from the
+          recommended ones
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Library;
--- src/pages/LibraryPage/LibraryPage.jsx
+++ src/pages/LibraryPage/LibraryPage.jsx
@@ -1,15 +1,19 @@
+import css from "../RecommendedPage/RecommendedPage.module.css";
+
 import AddBook from "../../components/AddBook/AddBook";
 import Dashboard from "../../components/Dashboard/Dashboard";
 import RecomBooksDash from "../../components/RecomBooksDash/RecomBooksDash";
+import Library from "../../components/Library/Library";
 
 const LibraryPage = () => {
   return (
-    <>
+    <div className={css.pageWrapper}>
       <Dashboard>
         <AddBook />
         <RecomBooksDash />
       </Dashboard>
-    </>
+      <Library />
+    </div>
   );
 };
 
