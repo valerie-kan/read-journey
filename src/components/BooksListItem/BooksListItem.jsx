340b77f735a9f751eccd7cf7316975385bc7feac
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-05-01 15:12:41 +0300

    add delete icon

--- src/components/BooksListItem/BooksListItem.jsx
+++ src/components/BooksListItem/BooksListItem.jsx
@@ -4,8 +4,9 @@ import css from "./BooksListItem.module.css";
 
 import BookModal from "../BookModal/BookModal";
 import GoodJobModal from "../GoodJobModal/GoodJobModal";
+import clsx from "clsx";
 
-const BooksListItem = ({ book }) => {
+const BooksListItem = ({ book, children }) => {
   const [isBookModalOpen, setIsBookModalOpen] = useState(false);
   const [isGJOpen, setIsGJOpen] = useState(false);
 
@@ -17,14 +18,26 @@ const BooksListItem = ({ book }) => {
     setIsGJOpen(true);
   };
 
+  const hasChildren = Boolean(children);
+
   return (
     <>
-      <li className={css.itemWrapper} onClick={() => setIsBookModalOpen(true)}>
-        <div className={css.imgCont}>
+      <li className={css.itemWrapper}>
+        <div className={css.imgCont} onClick={() => setIsBookModalOpen(true)}>
           <img className={css.bookImg} src={book.imageUrl} alt="Book cover" />
         </div>
-        <p className={css.bookTtl}>{book.title}</p>
-        <p className={css.bookAuthor}>{book.author}</p>
+        <div className={css.textAndIconWrapper}>
+          <div
+            className={clsx(
+              css.textWrapper,
+              hasChildren && css.narrowTextWrapper
+            )}
+          >
+            <p className={css.bookTtl}>{book.title}</p>
+            <p className={css.bookAuthor}>{book.author}</p>
+          </div>
+          {children}
+        </div>
       </li>
       {isBookModalOpen && (
         <BookModal
--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -2,19 +2,14 @@ import { useEffect, useState } from "react";
 
 import css from "./Library.module.css";
 
+import sprite from "../../assets/icons/symbol-defs.svg";
+
 import Select from "../Select/Select";
 import EmptyLibrary from "../EmptyLibrary/EmptyLibrary";
 import BooksListItem from "../BooksListItem/BooksListItem";
 
 const Library = () => {
-  const [books, setBooks] = useState(
-    JSON.parse(localStorage.getItem("my-library")) || []
-  );
-
-  // useEffect(() => {
-  //   const books = JSON.parse(localStorage.getItem("my-library"));
-  //   return books;
-  // }, [])
+  const handleDeleteClick = () => {};
 
   return (
     <div className={css.libWrapper}>
@@ -23,7 +18,21 @@ const Library = () => {
         <Select />
       </div>
       {books.length > 0 ? (
-        books.map((book) => <BooksListItem book={book} />)
+        <ul className={css.booksWrapper}>
+          {books.map((book) => (
+            <BooksListItem book={book}>
+              <div className={css.iconWrapper}>
+                <svg
+                  width={14}
+                  height={14}
+                  onClick={() => handleDeleteClick(book._id)}
+                >
+                  <use href={`${sprite}#icon-trash`} />
+                </svg>
+              </div>
+            </BooksListItem>
+          ))}
+        </ul>
       ) : (
         <EmptyLibrary />
       )}
